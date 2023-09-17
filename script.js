760dc85e6ae31e9ee480f0cf39a20d9efc84a666
BuildTools <unconfigured@null.spigotmc.org>
2023-09-17 15:52:46 -0600

    finished

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 var saveBtn = document.querySelectorAll('.saveBtn')
 
 $(function () {
@@ -13,12 +10,8 @@ $(function () {
   
   for (let i = 0; i < saveBtn.length; i++) { // Add event listener to all Save Buttons
     const element = saveBtn[i];
-    $(element).on('click', saveFunction)
+    $(element).on('click', save_to_storage)
   }
-
-
-
-
 
 
   // TODO: Add code to apply the past, present, or future class to each time
@@ -26,20 +19,61 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+
+  $('.container-lg').children().each((index, element) => {
+    console.log("hour: " + dayjs().hour() + "    " + "index: " + (index-(-10)))
+
+    if (dayjs().hour() > (index-(-10))) {
+
+      $(element).addClass('past')
+      // future
+      console.log("past")
+      
+    } else if (dayjs().hour() < (index-(-10))) {
+
+      $(element).addClass('future')
+      // past
+      console.log("future")
+
+    } else {
+
+      $(element).addClass('present')
+      //  present
+      console.log("present")
+    }
+  })
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  for (i = 0; i < 18; i++){
+    if (localStorage.getItem('hour-' + i)){
+      $('#hour-' + i).children()[1].innerText = localStorage.getItem('hour-' + i)
+    } else {
+    }
+    
+  }
+
+
   // TODO: Add code to display the current date in the header of the page.
+
+  $('#currentDay').text(dayjs().format('MMMM D, YYYY h:mm A'))
 });
 
-function saveFunction(e) {
-  var el = $(this).parent()
+function save_to_storage(e) {
+  console.log(this)
 
-  var name = el[0].id + ""
-  var val = el.children()[1].value + ""
+  for(f = 0; f < 5; f++){
 
-  localStorage.setItem(name, val)
+    var el = $(this).parent()
+    
 
+    var name = el.attr('id') + ""
+    var val = el.children()[1].value + ""
+
+    console.log(el.children())
+
+    localStorage.setItem(name, val)
+  }
 }
